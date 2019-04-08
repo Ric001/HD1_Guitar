b148ecee54fdd599596b814af5c8874c2bd73277
= <josericardo29@hotmail.es>
2019-04-08 00:18:40 -0400

    Initial Commit

---
+++ src/main/java/ricks/inventory_app/App.java
@@ -0,0 +1,14 @@
+package ricks.inventory_app;
+
+/**
+ * Hello world!
+ *
+ */
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+         
+    }
+}
---
+++ src/main/java/ricks/inventory_app/models/Guitar.java
@@ -0,0 +1,11 @@
+
+
+public class Guitar 
+{
+    private String serialNumber;
+    private String builder;
+    private String model;
+    private String type;
+    private String backWood;
+    private String topWood;
+}
